a8bd701e588ef270f50bcd4b8b2edcd57488165b
Boris Urman <urmanbm@gmail.com>
2021-04-08 01:18:20 +0300

    Celery working

--- statemanagement/admin.py
+++ statemanagement/admin.py
@@ -2,4 +2,4 @@ from django.contrib import admin
 
 from .models import StateContainer
 
-admin.register(StateContainer)
+admin.site.register(StateContainer)
--- takehomegai/settings.py
+++ takehomegai/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'django_extensions',
+    'django_celery_results',
     'rest_framework',
     'statemanagement'
 ]
@@ -46,7 +47,7 @@ MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
-    'django.middleware.csrf.CsrfViewMiddleware',
+    # 'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
@@ -121,3 +122,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+
+CELERY_BROKER_URL = "pyamqp://message-broker:5672"
+CELERY_RESULT_BACKEND = 'django-db'
